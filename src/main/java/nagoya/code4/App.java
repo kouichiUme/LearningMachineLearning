45c61bdcc2431a2160754c55a161c897bff2ed5e
kouichi <kouichiume@gmail.com>
2017-10-13 20:03:31 +0900

    main sample

--- src/main/java/nagoya/code4/App.java
+++ src/main/java/nagoya/code4/App.java
@@ -1,13 +1,16 @@
 package nagoya.code4;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaSparkContext;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		String master ="local";
+		SparkConf conf = new SparkConf().setAppName("sample noby " ).setMaster(master);
+		JavaSparkContext sc = new JavaSparkContext(conf);
+	}
 }
